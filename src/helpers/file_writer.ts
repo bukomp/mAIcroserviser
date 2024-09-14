a55ae8bed3819053e2d34c532cdf13a3cca613b4
bukomp <edvard@shalaev.com>
2024-09-14 05:00:27 +0300

    more minor refactorings

--- src/helpers/file_writer.ts
+++ src/helpers/file_writer.ts
@@ -1,11 +1,11 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import fs from 'fs';
+import path from 'path';
 import { config } from './config';
 import { filteroutModules } from './filters/requrements_filter';
 import { microserviceCount } from './microservice_counter';
 import { FileCollection, File } from '../models/file_interface';
 
-const writeFile = (filePath: string, content: string): void => {
+const _writeFile = (filePath: string, content: string): void => {
   try {
     fs.writeFileSync(filePath, content, { encoding: 'utf-8', flag: 'wx' });
   } catch (error) {
@@ -13,13 +13,13 @@ const writeFile = (filePath: string, content: string): void => {
   }
 };
 
-const writeMicroserviceFiles = (microserviceRootDir: string, fileContents: File[]): void => {
+const _writeMicroserviceFiles = (microserviceRootDir: string, fileContents: File[]): void => {
   fileContents.forEach((file) => {
     const content = file.name === 'requirements.txt' ? filteroutModules(file.content) : file.content;
     const filePath = path.join(process.cwd(), microserviceRootDir, file.path, file.name.trim());
 
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
-    writeFile(filePath, content);
+    _writeFile(filePath, content);
   });
 };
 
@@ -28,8 +28,8 @@ export const writeFilesFromCollection = (listOfFiles: FileCollection): void => {
     const microserviceRootDir = path.join(config.base_dir, `-${microserviceCount()}--${listOfFiles.projectRoot}`);
     const microserviceRawFilesRootDir = path.join(microserviceRootDir, 'raw_responses');
 
-    writeMicroserviceFiles(microserviceRootDir, listOfFiles.collections['formatted_responses'] ?? []);
-    writeMicroserviceFiles(microserviceRawFilesRootDir, listOfFiles.collections['raw_responses'] ?? []);
+    _writeMicroserviceFiles(microserviceRootDir, listOfFiles.collections['formatted_responses'] ?? []);
+    _writeMicroserviceFiles(microserviceRawFilesRootDir, listOfFiles.collections['raw_responses'] ?? []);
   } catch (error) {
     console.error('An error occurred while writing files:', error);
   }
